aedf1b9388686097b03ebe3b98f3e7be44197ba7
Cristina <cristinaanghel26@yahoo.com>
2024-04-21 18:22:50 +0300

    Initial springboot app setup

---
+++ comparator/src/main/java/etti/comparator/ComparatorAppApplication.java
@@ -0,0 +1,13 @@
+package etti.comparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparatorAppApplication.class, args);
+	}
+
+}
---
+++ comparator/src/test/java/etti/comparator/ComparatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package etti.comparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
