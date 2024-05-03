dd351e9b0498603db369f4f77dfdcc112b012f24
Cristina <cristinaanghel26@yahoo.com>
2024-05-04 00:31:56 +0300

    Login + Register

---
+++ comparator/src/main/java/etti/comparator/service/UserService.java
@@ -0,0 +1,10 @@
+package etti.comparator.service;
+import etti.comparator.dto.UserDto;
+import etti.comparator.model.User;
+
+public interface UserService {
+
+    User save (UserDto userDto);
+
+
+}
---
+++ comparator/src/main/java/etti/comparator/service/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package etti.comparator.service;
+
+import etti.comparator.dto.UserDto;
+import etti.comparator.model.User;
+import etti.comparator.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User save(UserDto userDto) {
+        User user = new User(userDto.getEmail(), passwordEncoder.encode(userDto.getPassword()) , "USER", userDto.getFullname());
+        return userRepository.save(user);
+    }
+
+}
